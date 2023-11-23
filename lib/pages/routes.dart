209e67f9025d98a3b2270ffb4b003078008665f1
Harish Navnit <harishnavnit@gmail.com>
2023-11-23 19:09:22 +0800

    add go_router

---
+++ lib/pages/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../pages/index.dart';
+import '../pages/favorites.dart' as favorites;
+
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  debugLogDiagnostics: true,
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => Index()),
+    GoRoute(path: '/favorites', builder: (context, state) => favorites.Display()),
+    GoRoute(path: '/favorites/add', builder: (context, state) => favorites.Create())
+  ]
+);
