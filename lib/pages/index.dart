71fedd953fa85a3af9ec74e57e6ca7d4ed4b12d8
Harish Navnit <harishnavnit@gmail.com>
2023-11-23 19:37:56 +0800

    Use navbar and canvas widgets

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:navidart/pages/favorites.dart';
+
+import '../widgets/canvas.dart';
+import '../widgets/navbar.dart';
 
 
 class Index extends StatefulWidget {
@@ -11,35 +13,22 @@ class Index extends StatefulWidget {
 class _IndexState extends State<Index> {
   var pageIndex = 0;
 
-  Widget active(index) {
-    switch (index) {
-      case 0: // Home page
-        return Placeholder();
-      case 1: // Show favorites
-        return Favorites();
-      default:
-        throw UnimplementedError("no widget defined for $index");
-    }
-  }
+  // Widget active(index) {
+  //   switch (index) {
+  //     case 0: // Home page
+  //       return Placeholder();
+  //     case 1: // Show favorites
+  //       return Favorites();
+  //     default:
+  //       throw UnimplementedError("no widget defined for $index");
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    Widget page = active(pageIndex);
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(body: Row(children: [
-            SafeArea(child: NavigationRail(
-                     extended: constraints.maxWidth >= 600,
-                     destinations: [NavigationRailDestination(icon: Icon(Icons.home), label: Text('Home'),),
-                                    NavigationRailDestination(icon: Icon(Icons.favorite), label: Text('Favorites'),),],
-                     selectedIndex: pageIndex,
-                     onDestinationSelected: (value) {
-                      setState(() { pageIndex = value; });
-                     },),
-            ),
-            Expanded(child: Container(color: Theme.of(context).colorScheme.primaryContainer, child: page,),),
-          ],
-        ),
-      );
+    return LayoutBuilder(builder: (BuildContext ctx, BoxConstraints constraints) {
+      return Scaffold(body: Row(children: [SafeArea(child: Navbar(constraints: constraints,)),
+                                           Expanded(child: Canvas())]));
     });
   }
 }
