998703becf64f24a79aa9490ecb289788e3e734f
Harish Navnit <harishnavnit@gmail.com>
2023-11-23 20:07:52 +0800

    Specify page for canvas

--- lib/widgets/canvas.dart
+++ lib/widgets/canvas.dart
@@ -5,16 +5,17 @@ import '../utils.dart';
 
 
 class Canvas extends StatefulWidget {
+  final Widget page;
+  const Canvas({required this.page, super.key}); // ctor
+
   @override
   State<Canvas> createState() => _CanvasState();
 }
 
 
 class _CanvasState extends State<Canvas> {
-  Widget _page = Placeholder();
-
   @override
   Widget build(BuildContext context) {
-    return container(_page, context);
+    return container(widget.page, context);
   }
 }
