9e82393a59820d9b9c3d43e1722ff5d1cf7fe11d
Harish Navnit <harishnavnit@gmail.com>
2023-11-23 19:36:37 +0800

    Rearrange pages/favorites

--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../utils.dart';
 import '../widgets/favorites/add.dart' show AddFavorites;
@@ -6,20 +7,54 @@ import '../widgets/favorites/show.dart' show ShowFavorites;
 import '../widgets/favorites/empty.dart' show EmptyFavorites;
 
 
-class Favorites extends StatefulWidget {
+/// Stateful widgets
+class Display extends StatefulWidget {
   @override
-  State<Favorites> createState() => _FavoritesState();
+  State<Display> createState() => _DisplayState();
+}
+
+class Create extends StatefulWidget {
+  @override
+  State<Create> createState() => _CreateState();
+}
+
+class Remove extends StatefulWidget {
+  @override
+  State<Remove> createState() => _RemoveState();
 }
 
 
-class _FavoritesState extends State<Favorites> {
+/// Stateful widget states
+class _DisplayState extends State<Display> {
+  @override
+  Widget build(BuildContext context) {
+    Map<String, String> params = GoRouterState.of(context).uri.queryParameters;
+    Widget page = params.keys.contains('empty') ? ShowFavorites() : EmptyFavorites();
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(body: Row(mainAxisSize: MainAxisSize.min,
+                                children: [container(page, context),],));
+    });
+  }
+}
+
+class _CreateState extends State<Create> {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(body: Row(mainAxisSize: MainAxisSize.min,
-                                children: [container(EmptyFavorites(), context),
-                                           container(AddFavorites(), context),
-                                           container(ShowFavorites(), context)],));
+                                children: [container(AddFavorites(), context)],));
+    });
+  }
+}
+
+// TODO: Implement remove screen {iff required}
+class _RemoveState extends State<Remove> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(body: Row(mainAxisSize: MainAxisSize.min,
+                                children: [container(EmptyFavorites(), context)],));
     });
   }
 }
