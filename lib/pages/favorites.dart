8393c882407f59c044b8103736760b4f3bab39e6
Harish Navnit <harishnavnit@gmail.com>
2023-12-03 02:49:24 +0800

    More sharedprefs

--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -1,6 +1,8 @@
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../widgets/canvas.dart';
 import '../widgets/navbar.dart';
@@ -29,6 +31,14 @@ class Display extends StatefulWidget {
 
 /// Stateful widget states
 class _CreateState extends State<Create> {
+  List<String> favorites = [];
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (BuildContext ctx, BoxConstraints constraints) {
@@ -36,17 +46,37 @@ class _CreateState extends State<Create> {
                                            Expanded(child: Canvas(page: AddFavorites(name: widget.name, phone: widget.phone)))]));
     });
   }
+
+  Future<void> load() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      favorites = prefs.getStringList('favorites') ?? [];
+    });
+  }
 }
 
 class _DisplayState extends State<Display> {
+  List<String> favorites = [];
+
+  @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Map<String, String> params = GoRouterState.of(context).uri.queryParameters;
-    Widget page = params.keys.contains('empty') ? ShowFavorites() : EmptyFavorites();
-
+    Widget page = favorites.isEmpty ? EmptyFavorites() : ShowFavorites();
     return LayoutBuilder(builder: (BuildContext ctx, BoxConstraints constraints) {
       return Scaffold(body: Row(children: [SafeArea(child: Navbar(constraints: constraints,)),
                                            Expanded(child: Canvas(page: page))]));
+    });
+  }
+
+  Future<void> load() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      favorites = prefs.getStringList('favorites') ?? [];
     });
   }
 }
