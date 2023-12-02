0994f59a6c9d8b1c68683f29488137d9f8063ae5
Harish Navnit <harishnavnit@gmail.com>
2023-12-02 20:38:32 +0800

    WIP

--- lib/widgets/favorites/add.dart
+++ lib/widgets/favorites/add.dart
@@ -4,9 +4,11 @@ import 'package:flutter/material.dart';
 import '../../utils.dart';
 import '../favorites/forms.dart';
 
+import '../../pages/favorites.dart' show Display;
+
 
 /// Add a new favorite
-class AddFavorites extends StatelessWidget {
+class AddFavorites extends StatefulWidget {
   final String? name;
   final String? phone;
 
@@ -14,8 +16,17 @@ class AddFavorites extends StatelessWidget {
   AddFavorites({super.key, this.name, this.phone});
 
   @override
+  State<AddFavorites> createState() => _AddFavoritesState();
+}
+
+class _AddFavoritesState extends State<AddFavorites> {
+  @override
   Widget build(BuildContext context) {
-    print('Adding a new favorite($name, $phone)');
-    return container(Center(child: NewFavoriteForm()), context);
+    if (widget.name == null || widget.phone == null) {
+      return container(Center(child: NewFavoriteForm()), context);
+    }
+
+    print('Adding a new favorite(${widget.name}, ${widget.phone})');
+    return container(Center(child: Display()), context);
   }
 }
