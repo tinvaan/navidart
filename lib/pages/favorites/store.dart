7da7f6d42d6dde6be18b82410a16eb0c47f2c903
Harish Navnit <harishnavnit@gmail.com>
2023-12-03 05:30:17 +0800

    more experiments

--- lib/pages/favorites/store.dart
+++ lib/pages/favorites/store.dart
@@ -1,50 +1,68 @@
 
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
-class Store {
+class Store extends State<StatefulWidget>{
   List<String> items = [];
   late SharedPreferences prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    sync();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError('Subclass responsibility');
+  }
 
   Future<void> sync() async {
     prefs = await SharedPreferences.getInstance();
     items = prefs.getStringList('favorites') ?? [];
   }
 
-  Future<List> all() async {
-    await sync();
+  List<String> all() {
+    sync();
     return items;
   }
 
-  Future<bool> empty() async {
-    await sync();
+  bool empty() {
+    sync();
     return items.isEmpty;
   }
 
-  Future<int> count() async {
-    await sync();
+  int count() {
+    sync();
     return items.length;
   }
 
   Future<void> clear() async {
     await sync();
-    items.clear();
-    prefs.setStringList('favorites', items);
+    setState(() {
+      items.clear();
+      prefs.setStringList('favorites', items);
+    });
   }
 
   Future<void> append(String name, String number) async {
     await sync();
-    items.add('$name,$number');
-    prefs.setStringList('favorites', items);
+    setState(() {
+      items.add('$name,$number');
+      prefs.setStringList('favorites', items);
+    });
   }
 
   Future<void> remove(String name, String? number) async {
     await sync();
-    items.removeWhere(
-      (fav) => fav.contains(name) || (number != null && fav.contains(number))
-    );
-    prefs.setStringList('favorites', items);
+    setState(() {
+      items.removeWhere(
+        (fav) => fav.contains(name) || (number != null && fav.contains(number))
+      );
+      prefs.setStringList('favorites', items);
+    });
   }
 }
