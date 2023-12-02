0eec5b054491b61da9ab454413f8048985da4960
Harish Navnit <harishnavnit@gmail.com>
2023-12-02 17:16:00 +0800

    Pass textfield inputs to widget as parameters

--- lib/widgets/favorites/forms.dart
+++ lib/widgets/favorites/forms.dart
@@ -4,6 +4,9 @@ import 'package:go_router/go_router.dart';
 
 
 class NewFavoriteForm extends StatelessWidget {
+  final name = TextEditingController();
+  final phone = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,17 +17,20 @@ class NewFavoriteForm extends StatelessWidget {
           SizedBox(height: 10),
           SizedBox(
             width: 250,
-            child: TextField(decoration: InputDecoration(border: OutlineInputBorder(), label: Text("Name")),)
+            child: TextField(controller: name, decoration: InputDecoration(border: OutlineInputBorder(), label: Text("Name")),)
           ),
           SizedBox(height: 10),
           SizedBox(
             width: 250,
-            child: TextField(decoration: InputDecoration(border: OutlineInputBorder(), label: Text("Phone No.")),)
+            child: TextField(controller: phone, decoration: InputDecoration(border: OutlineInputBorder(), label: Text("Phone No.")),)
           ),
           SizedBox(height: 10),
           FloatingActionButton(
+            tooltip: 'Please enter name and phone number',
             child: Icon(Icons.person_add),
-            onPressed: () => context.go('/favorites/add'),
+            onPressed: () => name.text.isEmpty || phone.text.isEmpty 
+              ? null
+              : context.go(Uri(path: '/favorites/add', queryParameters: {'name': name.text, 'phone': phone.text}).toString()),
           )
         ])
       )
