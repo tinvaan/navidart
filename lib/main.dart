7efa595b925d9f84d66851b5353f342bc351d84d
Harish Navnit <harishnavnit@gmail.com>
2023-12-02 21:30:04 +0800

    Drop AppState for shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_contacts/flutter_contacts.dart';
 
 import 'pages/routes.dart';
 
@@ -15,47 +12,6 @@ class App extends StatelessWidget {
     ThemeData style = ThemeData(useMaterial3: true,
                                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigoAccent));
     return MaterialApp.router(routerConfig: appRouter, theme: style);
-  }
-}
-
-
-class AppState extends ChangeNotifier {
-  List<Contact> favorites = [];
-  String current = String.fromEnvironment('DEFAULT_PHONE_NUMBER');
-
-  void add(String name, String number) {
-    print('Adding a new contact to favorites ...');
-    favorites.add(Contact(displayName: name, phones: [Phone(number)], notes: [Note('NAVIDART')]));
-    print('Favorites: $favorites');
-    print('Notifying listeners ...');
-    notifyListeners();
-  }
-
-  void remove(Contact addr) {
-    favorites.remove(addr);
-    notifyListeners();
-  }
-
-  Future addFavorites(String name, String number) async {
-    if (await FlutterContacts.requestPermission()) {
-      var contact = Contact(displayName: name, phones: [Phone(number)],
-                            notes: [Note("NAVIDART_CONTACT")]);
-      await contact.insert();
-      favorites.add(contact);
-    }
-    notifyListeners();
-  }
-
-  Future removeFavorite(Contact addr) async {
-    if (favorites.contains(addr)) {
-      favorites.remove(addr); // remove from AppState
-    }
-
-    if (await FlutterContacts.requestPermission()) {
-      await addr.delete(); // remove from contact list
-    }
-
-    notifyListeners();
   }
 }
 
